7194376ff1da4fe360e5fccd9ffbb3bf185414b6
José Ignacio Jeremías Aranda Baeza <jo.arandab@duocuc.cl>
2026-05-27 01:25:29 -0400

    Fix VPC module source and subnet ID reference
    Updated VPC module source reference and corrected subnet ID retrieval.

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 module "vpc" {
-  source = "github.com/Bas557169/AUY1105-Jose-Bastian-2//vpc_module?ref=VPC"
+  # Aseguramos que el tag coincida exactamente con el de GitHub (minúsculas si así lo creaste)
+  source = "github.com/Bas557169/AUY1105-Jose-Bastian-2//vpc_module?ref=vpc"
 }
 
 module "ec2" {
   source = "github.com/Bas557169/AUY1105-Jose-Bastian-2//ec2_module?ref=ec2"
 
   vpc_id        = module.vpc.vpc_id
-  subnet_id     = module.vpc.subnet_publica_1_id
+  
+  # CORRECCIÓN AQUÍ: Llamamos al output real y le pedimos la primera subnet de la lista
+  subnet_id     = module.vpc.public_subnet_ids[0] 
+  
   ami_id        = var.raiz_ami_id
   instance_type = var.raiz_instance_type
   environment   = var.raiz_environment
 }
-
-
